34edd8d8a06c2231f9b731504bc1e63fc9038f82
ANJIREDDY -Architect - AI-ML   [ BNYMELLON.COM -PUNE] <94956350+anjilinux@users.noreply.github.com>
2026-04-29 22:25:53 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -5,11 +5,6 @@ print("📊 Evaluating model...")
 accuracy = random.uniform(0.5, 1.0)
 print(f"Accuracy: {accuracy}")
 
-# save metrics locally
-with open("metrics.txt", "w") as f:
-    f.write(f"accuracy={accuracy}")
-
-# gate
 if accuracy < 0.8:
     raise Exception("❌ Model accuracy too low")
 
